27771718d790f0a333b788b096daab1235f2b2ad
김서준 <jskim332@naver.com>
2017-01-13 12:45:32 +0900

    초기

--- HelloWorld/src/com/test/hellowoerld/HelloWorld.java
+++ HelloWorld/src/com/test/hellowoerld/HelloWorld.java
@@ -3,6 +3,6 @@ package com.test.hellowoerld;
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hello, World~!");
-		System.out.println("안년하세요.");
+		System.out.println("안년하세요."); 
 	}
 }
